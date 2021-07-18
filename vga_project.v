c0ca16605d57747f198e64249d8b7f167b7dcfa1
KubaŚ <86601352+ProjektWOT@users.noreply.github.com>
2021-07-18 23:33:09 +0200

    Update vga_project.v

--- vga_project.v
+++ vga_project.v
@@ -1,6 +1,11 @@
 `timescale 1 ns / 1 ps
 
 module vga_example(
+    input wire MISO,
+    output wire SCLK,
+    output wire MOSI,
+    output wire CS,
+
     input wire clk,
     input wire rst,
     output wire vs,
@@ -16,42 +21,58 @@ module vga_example(
 
 //Mirror PCLK; Reset_Module; Clk_Module
 //***********************************************//
-wire clk_100MHz, clk_40MHz, Locked, RstExt;
+wire clk_100MHz, Locked, RstExt, clk_130MHz, clk_65MHz;
+
 ODDR pclk_oddr (
     .Q(pclk_mirror),
-    .C(clk_40MHz),
+    .C(clk_65MHz),
     .CE(1'b1),
     .D1(1'b1),
     .D2(1'b0),
     .R(1'b0),
     .S(1'b0)
-);
+); 
 Reset Reset(
     .locked(Locked),
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
     .reset(RstExt)
 );
 clk_wiz_0 Clock(
     .clk(clk),
     .reset(rst),
-    .clk100MHz(),
-    .clk65MHz(clk_40MHz),
-    .clk130MHz(clk_100MHz),
+    .clk100MHz(clk_100MHz),
+    .clk65MHz(clk_65MHz),
+    .clk130MHz(clk_130MHz),
     .locked(Locked)
 );
 //***********************************************//
 
-wire [11:0] rgb_out_map, rgb_out_menu, rgb_out_Base, rgb_OUT;
-wire [10:0] hcount_out_tim, hcount_out_Base;
-wire [9:0]  vcount_out_tim, vcount_out_Base;
-wire vsync_out_tim, hsync_out_tim, vsync_out_Base, hsync_out_Base, vsync_OUT, hsync_OUT;
-wire vblnk_out_tim, hblnk_out_tim, vblnk_out_Base, hblnk_out_Base;
+wire [11:0] rgb_out_map, rgb_out_menu, rgb_out_Base, rgb_OUT, rgb_tank;
+wire [10:0] hcount_out_tim, hcount_out_Base, hcount_tank;
+wire [9:0]  vcount_out_tim, vcount_out_Base, vcount_tank;
+wire vsync_out_tim, hsync_out_tim, vsync_out_Base, hsync_out_Base, vsync_OUT, hsync_OUT, hsync_tank, vsync_tank;
+wire vblnk_out_tim, hblnk_out_tim, vblnk_out_Base, hblnk_out_Base, hblnk_tank, vblnk_tank;
 
-wire [11:0] posX_40Mhz, posY_40Mhz, Out_posX_100MHz, Out_posY_100MHz, xpos_out_Base, ypos_out_Base;
-wire ButtonLeft, SelectMode;
+wire [11:0] posX_65Mhz, posY_65Mhz, Out_posX_130MHz, Out_posY_130MHz, xpos_out_Base, ypos_out_Base, xpos_tank, ypos_tank;
+wire ButtonLeft, SelectMode, SelectTank;
+
+wire [9:0] Data_Jstk_X, Data_Jstk_Y;
+wire [7:0] data_MISO;
+
+PMOD_JOYSTICK Joystick(
+    .clk(clk_130MHz),
+    .reset(RstExt),
+    .MISO(MISO),
+    
+    .MOSI(MOSI),
+    .SCLK(SCLK),
+    .CS(CS),
+    .Data_out_X(Data_Jstk_X),
+    .Data_out_Y(Data_Jstk_Y)
+    );
 
 vga_timing Timing (
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
     .rst(RstExt),
     
     .vcount(vcount_out_tim),
@@ -67,7 +88,7 @@ Draw_Map Mapa(
     .vcount_in(vcount_out_tim),
     .hblnk_in(hblnk_out_tim),
     .vblnk_in(vblnk_out_tim),
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
     .rst(RstExt),
 
     .rgb_out(rgb_out_map)
@@ -77,17 +98,17 @@ DrawMenu Menu(
     .vcount_in(vcount_out_tim),
     .hblnk_in(hblnk_out_tim),
     .vblnk_in(vblnk_out_tim),
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
     .rst(RstExt),
     
     .rgb_out(rgb_out_menu)
     );
 
 BaseControl BaseControl(
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
     .rst(RstExt),
-    .xpos(posX_40Mhz),
-    .ypos(posY_40Mhz),
+    .xpos(posX_65Mhz),
+    .ypos(posY_65Mhz),
     .ButtonLeft(ButtonLeft),
     .rgbMapa(rgb_out_map),
     .rgbMenu(rgb_out_menu),
@@ -102,16 +123,16 @@ BaseControl BaseControl(
 //Mouse
 
 MouseCtl MyMouse(
-    .clk(clk_100MHz),
+    .clk(clk_130MHz),
     .rst(RstExt),
     
-    .xpos(Out_posX_100MHz),
-    .ypos(Out_posY_100MHz),
+    .xpos(Out_posX_130MHz),
+    .ypos(Out_posY_130MHz),
     
     .ps2_clk(ps2_clk),
     .ps2_data(ps2_data),
-    .value(12'b001100011111),
-    .value_y(12'b001001010111),
+    .value(12'b001111111111),
+    .value_y(12'b001011111111),
     .setx(1'b0),
     .sety(1'b0),
     .setmax_x(1'b1),
@@ -122,17 +143,40 @@ MouseCtl MyMouse(
     .middle(),
     .new_event()
     );
-Mouse_100To40 Mouse_100To40(
-    .clk(clk_40MHz),
+Mouse_130To65 Mouse_130To65(
+    .clk(clk_65MHz),
     .rst(RstExt),
-    .B_posX(Out_posX_100MHz),
-    .B_posY(Out_posY_100MHz),
+    .B_posX(Out_posX_130MHz),
+    .B_posY(Out_posY_130MHz),
     
-    .posX(posX_40Mhz),
-    .posY(posY_40Mhz)
+    .posX(posX_65Mhz),
+    .posY(posY_65Mhz)
     );
 MouseImage Cursor(
-    .clk(clk_40MHz),
+    .clk(clk_65MHz),
+    .rst(RstExt),
+    .xpos(xpos_tank),
+    .ypos(ypos_tank),
+    .hcount(hcount_tank),
+    .vcount(vcount_tank),
+    .hblnk(hblnk_tank),
+    .vblnk(vblnk_tank),
+    .hsync(hsync_tank),
+    .vsync(vsync_tank),
+    .rgb_in(rgb_tank),
+    .SelectMode(SelectTank),
+    
+    .hsync_out(hsync_OUT),
+    .vsync_out(vsync_OUT),
+    .hblnk_out(),
+    .vblnk_out(),
+    .hcount_out(),
+    .vcount_out(),
+    .rgb_out(rgb_OUT)
+    );
+    
+Tank_Gen Tank_Gen(
+    .clk(clk_65MHz),
     .rst(RstExt),
     .xpos(xpos_out_Base),
     .ypos(ypos_out_Base),
@@ -144,14 +188,19 @@ MouseImage Cursor(
     .vsync(vsync_out_tim),
     .rgb_in(rgb_out_Base),
     .SelectMode(SelectMode),
-    
-    .hsync_out(hsync_OUT),
-    .vsync_out(vsync_OUT),
-    .hblnk_out(),
-    .vblnk_out(),
-    .hcount_out(),
-    .vcount_out(),
-    .rgb_out(rgb_OUT)
+    .Data_in_X(Data_Jstk_X),
+    .Data_in_Y(Data_Jstk_Y),
+       
+    .hsync_out(hsync_tank),
+    .vsync_out(vsync_tank),
+    .hblnk_out(hblnk_tank),
+    .vblnk_out(vblnk_tank),
+    .hcount_out(hcount_tank),
+    .vcount_out(vcount_tank),
+    .rgb_out(rgb_tank),
+    .Select_out(SelectTank),
+    .xpos_out(xpos_tank),
+    .ypos_out(ypos_tank)
     );
 //***********************************************//
 

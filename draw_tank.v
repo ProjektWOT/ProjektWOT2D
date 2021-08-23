f0fe5dd4fcb2723899e6a56b72698a41db74cc25
KubaŚ <86601352+ProjektWOT@users.noreply.github.com>
2021-08-23 21:29:10 +0200

    Update draw_tank.v

--- draw_tank.v
+++ draw_tank.v
@@ -10,10 +10,14 @@ module draw_tank(
     input wire vsync_in,
     input wire hblnk_in,
     input wire vblnk_in,
-    input wire [11:0] posX,
-    input wire [11:0] posY,
+    input wire [9:0] posX,
+    input wire [9:0] posY,
     input wire [11:0] rgb_in,
-    input wire [11:0] rgb_pixel,
+    input wire [11:0] rgb_pixel_0,
+    input wire [11:0] rgb_pixel_1,
+    input wire [11:0] rgb_pixel_2,
+    input wire [11:0] rgb_pixel_3,
+    input wire [1:0] direction_bullet,
     
     output reg [10:0] hcount_out,
     output reg [9:0] vcount_out,
@@ -23,14 +27,18 @@ module draw_tank(
     output reg vblnk_out,
     output reg [11:0] rgb_out,
     output reg select_out,
+    output reg [9:0] posX_out_tank,
+    output reg [9:0] posY_out_tank,
+    output reg [1:0] direction_bullet_out,
     output [11:0] pixel_addr
     );
     
 wire [5:0] Addr_x, Addr_y;
 
+reg [1:0] direction_bullet_temp;
 reg [11:0] rgb_temp, rgb_out_nxt;
 reg [10:0] hcount_temp;
-reg [9:0] vcount_temp;
+reg [9:0] vcount_temp, posX_temp, posY_temp;
 reg hsync_temp, vsync_temp, hblnk_temp, vblnk_temp, select_temp;
 
 always@ (posedge clk)
@@ -47,6 +55,9 @@ else begin
     vcount_temp <= vcount_in;
     rgb_temp <= rgb_in;
     select_temp <= select;
+    posX_temp <= posX;
+    posY_temp <= posY;
+    direction_bullet_temp <= direction_bullet;
     
     hsync_out <= hsync_temp;
     vsync_out <= vsync_temp;
@@ -56,17 +67,42 @@ else begin
     vcount_out <= vcount_temp;
     rgb_out <= rgb_out_nxt;
     select_out <= select_temp;
+    posX_out_tank <= posX_temp;
+    posY_out_tank <= posY_temp;
+    direction_bullet_out <= direction_bullet_temp;
 end
 
 localparam LENGTH = 48;
 localparam HEIGTH = 64;
 
 always@* begin
-if(select == 0) rgb_out_nxt = rgb_temp;
-else if (rgb_pixel == 12'hf_f_f) rgb_out_nxt = rgb_temp;
-else if (vcount_temp>=posY && vcount_temp<(posY+HEIGTH) && hcount_temp>=posX && hcount_temp<(posX+LENGTH) && hblnk_temp==0 && vblnk_temp==0) rgb_out_nxt = rgb_pixel;
-else rgb_out_nxt = rgb_temp;
+    if(direction_bullet==0) begin
+        if(select == 0) rgb_out_nxt = rgb_temp;
+        else if (rgb_pixel_0 == 12'hf_f_f) rgb_out_nxt = rgb_temp;
+        else if (vcount_temp>=posY && vcount_temp<(posY+HEIGTH) && hcount_temp>=posX && hcount_temp<(posX+LENGTH) && hblnk_temp==0 && vblnk_temp==0) rgb_out_nxt = rgb_pixel_0;
+        else rgb_out_nxt = rgb_temp;
+    end
+    else if(direction_bullet==1) begin
+        if(select == 0) rgb_out_nxt = rgb_temp;
+        else if (rgb_pixel_1 == 12'hf_f_f) rgb_out_nxt = rgb_temp;
+        else if (vcount_temp>=posY && vcount_temp<(posY+HEIGTH) && hcount_temp>=posX && hcount_temp<(posX+LENGTH) && hblnk_temp==0 && vblnk_temp==0) rgb_out_nxt = rgb_pixel_1;
+        else rgb_out_nxt = rgb_temp;
+    end   
+    else if(direction_bullet==2) begin
+        if(select == 0) rgb_out_nxt = rgb_temp;
+        else if (rgb_pixel_2 == 12'hf_f_f) rgb_out_nxt = rgb_temp;
+        else if (vcount_temp>=posY && vcount_temp<(posY+LENGTH) && hcount_temp>=posX && hcount_temp<(posX+HEIGTH) && hblnk_temp==0 && vblnk_temp==0) rgb_out_nxt = rgb_pixel_2;
+        else rgb_out_nxt = rgb_temp;
+    end  
+    else if(direction_bullet==3) begin
+        if(select == 0) rgb_out_nxt = rgb_temp;
+        else if (rgb_pixel_3 == 12'hf_f_f) rgb_out_nxt = rgb_temp;
+        else if (vcount_temp>=posY && vcount_temp<(posY+LENGTH) && hcount_temp>=posX && hcount_temp<(posX+HEIGTH) && hblnk_temp==0 && vblnk_temp==0) rgb_out_nxt = rgb_pixel_3;
+        else rgb_out_nxt = rgb_temp;
+    end  
+    else rgb_out_nxt = rgb_temp;
 end
+
 
 assign Addr_y = vcount_in - posY;
 assign Addr_x = hcount_in - posX;

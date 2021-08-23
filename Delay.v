d43d9caebdc2739323b20d309bc60ce3e2cb91ab
KubaŚ <86601352+ProjektWOT@users.noreply.github.com>
2021-08-23 21:27:06 +0200

    Update Delay.v

--- Delay.v
+++ Delay.v
@@ -2,16 +2,22 @@
 
 module Delay(
     input wire clk,
-
+    input wire [9:0] Data_X_in,
+    input wire [9:0] Data_Y_in,
     input wire [11:0] xpos,
     input wire [11:0] ypos,
-    
+ 
+    output reg [9:0] Data_X_out,
+    output reg [9:0] Data_Y_out,
     output reg [11:0] xpos_out,
     output reg [11:0] ypos_out  
     );  
     
     always@(posedge clk) begin
+            Data_X_out <= Data_X_in;
+            Data_Y_out <= Data_Y_in;
             xpos_out <= xpos;
             ypos_out <= ypos;
         end
 endmodule
+

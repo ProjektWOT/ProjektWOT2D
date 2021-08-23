437271d36d0273a275e7ab553ee1982016326606
KubaŚ <86601352+ProjektWOT@users.noreply.github.com>
2021-08-23 21:33:45 +0200

    Update Tank_Gen.v

--- Tank_Gen.v
+++ Tank_Gen.v
@@ -15,6 +15,9 @@ module Tank_Gen(
     input wire SelectMode,
     input wire [9:0] Data_in_X,
     input wire [9:0] Data_in_Y,
+    input wire [9:0] Data_X_op,
+    input wire [9:0] Data_Y_op,
+    input wire left_click,
     
     output wire hsync_out,
     output wire vsync_out,
@@ -25,32 +28,30 @@ module Tank_Gen(
     output wire [11:0] rgb_out,
     output wire Select_out,
     output wire [11:0] xpos_out,
-    output wire  [11:0] ypos_out 
+    output wire [11:0] ypos_out,
+    output wire [9:0] xpos_UART,
+    output wire [9:0] ypos_UART
 );
 
-wire [11:0] xposTank, yposTank, xpos_d, ypos_d, rgb_ctl, Address, rgb_image;
-wire [10:0]  hcount_ctl; //, hcount_d;
-wire [9:0]  vcount_ctl; //, vcount_d;
-wire hsync_ctl, vsync_ctl, hblnk_ctl, vblnk_ctl, Select_ctl; //, hsync_d, vsync_d, hblnk_d, vblnk_d;
-    
+wire [11:0] rgb_image0, rgb_image1, rgb_image2, rgb_image3, rgb_draw, rgb_gun;
+wire [11:0] xposTank, yposTank, xpos_d, ypos_d, rgb_ctl, Address, xpos_draw, ypos_draw, xpos_gun, ypos_gun;
+wire [10:0]  hcount_ctl, hcount_draw, hcount_gun; //, hcount_d;
+wire [9:0]  vcount_ctl, vcount_draw, vcount_gun; //, vcount_d;
+wire hsync_ctl, vsync_ctl, hblnk_ctl, vblnk_ctl, Select_ctl, Select_draw, hblnk_draw, vblnk_draw, hsync_draw, vsync_draw,
+hblnk_gun, vblnk_gun, hsync_gun, vsync_gun; 
+wire [1:0] direction_bullet, direction_bullet_draw;
+wire [9:0] Data_X_op_out, Data_Y_op_out, posX_drawtank, posY_drawtank;
+wire tank_our_hit; 
+
 Delay Delay(
     .clk(clk),
-    //.rst(rst),
     .xpos(xpos),
     .ypos(ypos),
-    //.hcount(hcount),
-    //.vcount(vcount),
-    //.hblnk(hblnk),
-    //.vblnk(vblnk),
-    //.hsync(hsync),
-    //.vsync(vsync),
-        
-    //.hcount_out(hcount_d),
-    //.vcount_out(vcount_d),
-    //.hsync_out(hsync_d),
-    //.vsync_out(vsync_d),
-    //.hblnk_out(hblnk_d),
-    //.vblnk_out(vblnk_d),
+    .Data_X_in(Data_X_op),
+    .Data_Y_in(Data_Y_op),
+    
+    .Data_X_out(Data_X_op_out),
+    .Data_Y_out(Data_Y_op_out),
     .xpos_out(xpos_d),
     .ypos_out(ypos_d)  
 );
@@ -68,7 +69,9 @@ Control Control(
     .rgb_in(rgb_in),
     .Data_in_X(Data_in_X),
     .Data_in_Y(Data_in_Y),
-        
+    .Data_X_op(Data_X_op_out),
+    .Data_Y_op(Data_Y_op_out),
+    
     .Select_out(Select_ctl),
     .hcount_out(hcount_ctl),
     .vcount_out(vcount_ctl),
@@ -77,8 +80,9 @@ Control Control(
     .hblnk_out(hblnk_ctl),
     .vblnk_out(vblnk_ctl),
     .rgb_out(rgb_ctl),
-    .xpos(xposTank),
-    .ypos(yposTank)  
+    .xpos_UART(xpos_UART),
+    .ypos_UART(ypos_UART),
+    .direction_bullet(direction_bullet)
 );
 
 draw_tank DrawTank(
@@ -92,26 +96,35 @@ draw_tank DrawTank(
     .hblnk_in(hblnk_ctl),
     .vblnk_in(vblnk_ctl),
     .rgb_in(rgb_ctl),
-    .rgb_pixel(rgb_image),
-    .posX(xposTank),
-    .posY(yposTank),
+    .rgb_pixel_0(rgb_image0),
+    .rgb_pixel_1(rgb_image1),
+    .rgb_pixel_2(rgb_image2),
+    .rgb_pixel_3(rgb_image3),
+    .posX(xpos_UART),
+    .posY(ypos_UART),
+    .direction_bullet(direction_bullet),
     
-    .hcount_out(hcount_out),
-    .hsync_out(hsync_out),
-    .hblnk_out(hblnk_out),
-    .vcount_out(vcount_out),
-    .vsync_out(vsync_out),
-    .vblnk_out(vblnk_out),
-    .select_out(Select_out),
-    .rgb_out(rgb_out),
-    .pixel_addr(Address)
+    .hcount_out(hcount_draw),
+    .hsync_out(hsync_draw),
+    .hblnk_out(hblnk_draw),
+    .vcount_out(vcount_draw),
+    .vsync_out(vsync_draw),
+    .vblnk_out(vblnk_draw),
+    .select_out(Select_draw),
+    .rgb_out(rgb_draw),
+    .pixel_addr(Address),
+    .posX_out_tank(posX_drawtank),
+    .posY_out_tank(posY_drawtank),
+    .direction_bullet_out(direction_bullet_draw)
 );
-
-image_tank ImageTank(
+ChoseTheImageOfTank ChoseTheImageOfTank(
     .clk(clk),
     .address(Address),
-    
-    .rgb(rgb_image)
+
+    .rgb0(rgb_image0),
+    .rgb1(rgb_image1),
+    .rgb2(rgb_image2),
+    .rgb3(rgb_image3)
 );
 
 DelayForDraw DelayForDraw(
@@ -119,7 +132,72 @@ DelayForDraw DelayForDraw(
     .xposMouse(xpos_d),
     .yposMouse(ypos_d),
     
-    .xpos(xpos_out),
-    .ypos(ypos_out)
+    .xpos(xpos_draw),
+    .ypos(ypos_draw)
+);
+
+Gun_ctrl Gun_ctrl(
+    .clk(clk),
+    .rst(rst),
+    .left_click(left_click),
+    .select(Select_draw),
+    .hblnk(hblnk_draw),
+    .vblnk(vblnk_draw),
+    .hsync(hsync_draw),
+    .vsync(vsync_draw),
+    .hcount(hcount_draw),
+    .vcount(vcount_draw),
+    .rgb(rgb_draw),
+    .xpos_m(xpos_draw),
+    .ypos_m(ypos_draw),
+    .xpos_t(posX_drawtank),
+    .ypos_t(posY_drawtank),
+    .xpos_t_op(Data_X_op_out),
+    .ypos_t_op(Data_Y_op_out),
+    .direction_bullet(direction_bullet_draw),
+    
+    .select_out(Select_out),
+    .hblnk_out(hblnk_gun),
+    .vblnk_out(vblnk_gun),
+    .hsync_out(hsync_gun),
+    .vsync_out(vsync_gun),
+    .hcount_out(hcount_gun),
+    .vcount_out(vcount_gun),
+    .rgb_out(rgb_gun),
+    .xpos_m_out(xpos_gun),
+    .ypos_m_out(ypos_gun),
+    .tank_central_hit(tank_our_hit),
+    .xpos_bullet_green(),
+    .ypos_bullet_green()
+);
+
+Opponent_Bullet Opponent_Bullet(
+    .clk(clk),
+    .rst(rst),
+    .xpos_bullet_red(),
+    .ypos_bullet_red(),
+    .hblnk(hblnk_gun),
+    .vblnk(vblnk_gun),
+    .hsync(hsync_gun),
+    .vsync(vsync_gun),
+    .hcount(hcount_gun),
+    .vcount(vcount_gun),
+    .rgb(rgb_gun),
+    .xpos_m(xpos_gun),
+    .ypos_m(ypos_gun),
+    .tank_enemy_hit_us(),
+    .tank_our_hit(tank_our_hit),
+    
+    .hblnk_out(hblnk_out),
+    .vblnk_out(vblnk_out),
+    .hsync_out(hsync_out),
+    .vsync_out(vsync_out),
+    .hcount_out(hcount_out),
+    .vcount_out(vcount_out),
+    .rgb_out(rgb_out),
+    .xpos_m_out(xpos_out),
+    .ypos_m_out(ypos_out),
+    .tank_enemy_hit_us_out(),
+    .tank_our_hit_out()
 );
 endmodule

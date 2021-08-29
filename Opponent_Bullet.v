bf8c98e348a929f46645246d2b76bf9580c3cb10
KubaŚ <86601352+ProjektWOT@users.noreply.github.com>
2021-08-29 23:01:53 +0200

    Update Opponent_Bullet.v

--- Opponent_Bullet.v
+++ Opponent_Bullet.v
@@ -6,7 +6,6 @@ module Opponent_Bullet(
     input wire [9:0] xpos_bullet_red,
     input wire [9:0] ypos_bullet_red,
     input wire tank_enemy_hit_us,
-    input wire tank_our_hit,
     input wire hblnk,
     input wire vblnk,
     input wire hsync,
@@ -16,6 +15,7 @@ module Opponent_Bullet(
     input wire [11:0] rgb,
     input wire [11:0] xpos_m,
     input wire [11:0] ypos_m,
+    input wire [2:0] direction_from_enemy,
         
     output reg hblnk_out,
     output reg vblnk_out,
@@ -25,15 +25,16 @@ module Opponent_Bullet(
     output reg [9:0]  vcount_out,
     output reg [11:0] rgb_out,
     output reg [11:0] xpos_m_out,
-    output reg [11:0] ypos_m_out,
-    output reg tank_enemy_hit_us_out,
-    output reg tank_our_hit_out
+    output reg [11:0] ypos_m_out
 );
  
 reg [11:0] rgb_nxt;
-reg state, state_nxt;
-localparam IDLE = 1'b0;
-localparam FLY  = 1'b1;
+reg [2:0] state, state_nxt;
+localparam IDLE = 3'b000;
+localparam GEN_SHELL_0  = 3'b001;
+localparam GEN_SHELL_1  = 3'b010;
+localparam GEN_SHELL_2  = 3'b011;
+localparam GEN_SHELL_3  = 3'b100;
 
 always@(posedge clk) begin
     if(rst) begin
@@ -41,7 +42,6 @@ always@(posedge clk) begin
         {hcount_out, vcount_out, rgb_out} <= 0;
         xpos_m_out <= xpos_m;
         ypos_m_out <= ypos_m;
-        {tank_enemy_hit_us_out, tank_our_hit_out} <= 0;
         state <= IDLE;
     end
     else begin
@@ -54,9 +54,7 @@ always@(posedge clk) begin
         vcount_out <= vcount; 
         rgb_out <= rgb_nxt;
         xpos_m_out <= xpos_m;
-        ypos_m_out <= ypos_m;
-        tank_enemy_hit_us_out <= tank_enemy_hit_us;
-        tank_our_hit_out <= tank_our_hit;         
+        ypos_m_out <= ypos_m;     
     end
 end
 
@@ -64,20 +62,38 @@ always@* begin
     state_nxt = state;
     rgb_nxt = rgb_out;
     case(state)
-        IDLE: begin
-            if(xpos_bullet_red == 0 && ypos_bullet_red == 0) begin
-                state_nxt = IDLE;
-                rgb_nxt = rgb;
-            end
-            else begin
-                state_nxt = FLY;
-                rgb_nxt = rgb;
-            end
-        end
-        FLY: begin
+    IDLE: begin
+        if(direction_from_enemy == 0) begin
+            state_nxt = IDLE;
             rgb_nxt = rgb;
+            end
+        else begin
+            state_nxt = IDLE + direction_from_enemy;
+            //state_nxt = IDLE;
+            rgb_nxt = rgb;
+            end
         end
-        default: state_nxt =IDLE;
+    GEN_SHELL_0: begin
+        if((tank_enemy_hit_us == 1)||(direction_from_enemy == 0)) begin state_nxt = IDLE; rgb_nxt = rgb; end
+        else if(hcount >= xpos_bullet_red - 2 && hcount <= xpos_bullet_red + 2 && vcount >= ypos_bullet_red - 5 && vcount <= ypos_bullet_red + 5) rgb_nxt=12'h000;
+        else rgb_nxt = rgb;
+        end
+    GEN_SHELL_1: begin
+        if((tank_enemy_hit_us == 1)||(direction_from_enemy == 0)) begin state_nxt = IDLE; rgb_nxt = rgb; end
+        else if(hcount >= xpos_bullet_red - 2 && hcount <= xpos_bullet_red + 2 && vcount >= ypos_bullet_red + 19 && vcount <= ypos_bullet_red + 29) rgb_nxt=12'h000;
+        else rgb_nxt = rgb;
+        end
+    GEN_SHELL_2: begin
+        if((tank_enemy_hit_us == 1)||(direction_from_enemy == 0)) begin state_nxt = IDLE; rgb_nxt = rgb; end
+        else if(hcount >= xpos_bullet_red + 19 && hcount <= xpos_bullet_red + 29 && vcount >= ypos_bullet_red - 2 && vcount <= ypos_bullet_red + 2) rgb_nxt=12'h000;
+        else rgb_nxt = rgb;
+        end
+    GEN_SHELL_3: begin
+        if((tank_enemy_hit_us == 1)||(direction_from_enemy == 0)) begin state_nxt = IDLE; rgb_nxt = rgb; end
+        else if(hcount >= xpos_bullet_red - 5 && hcount <= xpos_bullet_red + 5 && vcount >= ypos_bullet_red - 2 && vcount <= ypos_bullet_red + 2) rgb_nxt=12'h000;
+        else rgb_nxt = rgb;
+        end
+    default: begin state_nxt =IDLE; rgb_nxt = rgb; end
     endcase
 end
 endmodule
